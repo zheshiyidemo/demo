8a92b8b273679f8b2708f07e4d3da5b7540e8145
hubiao <432432@qq.com>
2017-04-27 16:56:11 +0800

    peiz

---
+++ config-server/src/main/java/com/cc/server/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.cc.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by hubiao on 2017/4/26.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
